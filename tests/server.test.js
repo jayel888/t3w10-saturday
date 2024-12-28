95788623699b818539d0c2cff8b563df6fa6316f
jayel888 <15644@coderacademy.edu.au>
2024-12-28 18:22:40 +1100

    feat: feat: Built our first test case to test on the root route GET

---
+++ tests/server.test.js
@@ -0,0 +1,18 @@
+//  Importing the code that we want to test
+
+const { app } = require("../src/server.js");
+// Importing a testing helper function from supertest
+const request = require("supertest")
+
+//  Syntax: describe(desc, callback test function)
+describe("Root route", () => {
+    // Syntax: test/it (acknowledgement msg, callback)
+    test("Server returns 'Hello World!' message", async () => {
+        //  Make a request to the '/' route
+        // wait for response and store it
+        const response = await request(app).get("/");
+        
+        // check the body of the response to see if it matches the 'Hello World!' string
+        expect(response.body.message).toBe("Hello World!");
+    })
+});
